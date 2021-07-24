8186346ca07f18c5313144b55fead648849cdf2e
Richard Lin <rlin24@berkeley.edu>
2021-07-23 19:48:32 -0700

    reverted to commit 441c15ff699c8f500b169da8cc0d8cfaa46c4aa7 because previous commit was glitchy

--- Sequencer2021/js/courses.js
+++ Sequencer2021/js/courses.js
@@ -1,10 +1,9 @@
+// same file as commit 441c15ff699c8f500b169da8cc0d8cfaa46c4aa7
 CourseListing([
-  // CORE CLASSES + BREADTHS
   {
     "id": 0,//
     "name": "Opt Out",
     "title": "Take no course for this term",
-    "type":"NA"
     "term": -1,
     "series": 0,
     "inSequence": false,
@@ -17,7 +16,6 @@ CourseListing([
     "id": 1,//
     "name": "PHW200G",
     "title": "Health and Social Behavior",
-    "type":"Core/Breadth",
     "term": 0,
     "series": 0,
     "inSequence": false,
@@ -28,10 +26,239 @@ CourseListing([
   },
   {
     "id": 2,//
-    "name": "PHW200E",
-    "title": "Health Policy and Management",
-    "type":"Core/Breadth",
+    "name": "PHW224",
+    "title": "Organizational Behavior and Management in Health Care",
+    "term": 1,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+{
+    "id": 3,//
+    "name": "00000",
+    "title": "placeholder",
+    "term": 10,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+
+    {
+    "id": 4,//
+    "name": "PHW260",
+    "title": "Infectious Disease",
+    "term": 1,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+  {
+    "id": 5,//
+    "name": "PHW250B",
+    "title": "Epidemiologic Methods II",
     "term": 0,
+    "series": 1,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":4,
+    "weeks":15
+  },
+  {
+    "id": 6,//
+    "name": "PHW250",
+    "title": "Epidemiologic Methods",
+    "term": 0,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":0,
+    "credits":3,
+    "weeks":15
+  },
+  {
+    "id": 7,//
+    "name": "PHW226A",
+    "title": "Health Economics",
+    "term": 2,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+  {
+    "id": 8,//
+    "name": "PHW272C",
+    "title": "Applied Spatial Data Science for Public Health",
+    "term": 5,
+    "series": 2,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+  {
+    "id": 9,//
+    "name": "PHW213",
+    "title": "Global Health Ethics",
+    "term": 2,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+{
+    "id": 10,//
+    "name": "00000",
+    "title": "placeholder",
+    "term": 10,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+
+   {
+    "id": 11,//
+    "name": "PHW200F",
+    "title": "Introduction to Environmental Health Sciences",
+    "term": 3,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":0,
+    "credits":2,
+    "weeks":15
+  },
+  {
+    "id": 12,//
+    "name": "PHW218",
+    "title": "Evaluation of Health and Social Programs",
+    "term": 5,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+  {
+    "id": 13,//
+    "name": "PHW226C",
+    "title": "Economics of Population Health",
+    "term": 4,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+  {
+    "id": 14,//
+    "name": "PHW212",
+    "title": "Foundations of Global Health",
+    "term": 1,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+  {
+    "id": 15,//
+    "name": "PHW272A",
+    "title": "Intro to GIS for Public Health",
+    "term": 4,
+    "series": 2,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+  {
+    "id": 16,//
+    "name": "PHW142",
+    "title": "Introduction to Probability and Statistics",
+    "term": 3,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":0,
+    "credits":4,
+    "weeks":15
+  },
+  {
+    "id": 17,//
+    "name": "PHW219",
+    "title": "Social & Behavioral Health: Intro to Survey Methods",
+    "term": 4,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+  {
+    "id": 18,//
+    "name": "PHW223",
+    "title": "Strategy in Health Care Organizations",
+    "term": 8,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":7
+  },
+  {
+    "id": 19,
+    "name": "PHW277",
+    "title": "GIS for Public Health Practice",
+    "term": 5,
+    "series": 2,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+  {
+    "id": 20,//
+    "name": "PHW200E",
+    "title": "Introduction to Health Policy and Management",
+    "term": 7,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":0,
+    "credits":3,
+    "weeks":15
+  },
+  {
+    "id": 21,//
+    "name": "PHW289",
+    "title": "Interdisciplinary Seminar",
+    "term": 7,
     "series": 0,
     "inSequence": false,
     "link":"link goes here",
@@ -39,12 +266,215 @@ CourseListing([
     "credits":3,
     "weeks":7
   },
-{
-    "id": 3,//
-    "name": "PHW200F",
-    "title": "Intro to Environmental Health Science",
-    "type":"Core/Breadth"
+  {
+    "id": 22,//
+    "name": "PHW257",
+    "title": "Public Health Preparedness and Emergency Response",
+    "term": 7,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":7
+  },
+  {
+    "id": 23,//
+    "name": "PHW204",
+    "title": "Mass Communication in Public Health",
+    "term": 8,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":7
+  },
+  {
+    "id": 24,//
+    "name": "PHW206",
+    "title": "Maternal and Child Health Nutrition",
+    "term": 8,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":7
+  },
+  {
+    "id": 25,//
+    "name": "PHW227A",
+    "title": "Health Care Finance",
+    "term": 5,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+  {
+    "id": 26,//
+    "name": "PHW209",
+    "title": "Comparative Health Systems",
+    "term": 4,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+  {
+    "id": 27,//
+    "name": "PHW253",
+    "title": "Outbreak Investigations",
+    "term": 2,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+  {
+    "id": 28,//
+    "name": "PHW236A",
+    "title": "Regulatory Science, Drug Development and Public Health",
+    "term": 2,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+  {
+    "id": 29,
+    "name": "PHW202",
+    "title": "Ethnic and Cultural Diversity in Health Status and Behavior",
+    "term": 10,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":15
+  },
+  {
+    "id": 30,//
+    "name": "PHW252",
+    "title": "Epidemiological Analysis",
     "term": 0,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":0,
+    "credits":4,
+    "weeks":15
+  },
+  {
+    "id": 31,//
+    "name": "PHW290",
+    "title": "R for Public Health",
+    "term": 0,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":0,
+    "credits":2,
+    "weeks":15
+  },
+  {
+    "id": 32,//
+    "name": "PHW220M",
+    "title": "Health Policy Methods",
+    "term": 1,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":8
+  },
+  {
+    "id": 33,//
+    "name": "PHW241R",
+    "title": "Categorical Analysis",
+    "term": 3,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":4,
+    "weeks":15
+  },
+  {
+    "id": 34,//
+    "name": "PHW252C",
+    "title": "Intervention Trial Design",
+    "term": 3,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":0,
+    "credits":3,
+    "weeks":15
+  },
+  {
+    "id": 35,//
+    "name": "PHW226F",
+    "title": "Cost Effectiveness Analysis",
+    "term": 4,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":1,
+    "weeks":4
+  },
+{
+    "id": 36,//
+    "name": "placeholder",
+    "term": 10,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":1,
+    "weeks":4
+  },
+
+    {
+    "id": 37,//
+    "name": "PHW205",
+    "title": "Program Planning, Development, and Evaluation",
+    "term": 8,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":1,
+    "credits":3,
+    "weeks":7
+  },
+  {
+    "id": 38,//
+    "name": "Placeholder",
+    "title": "38 was missing so I added this placeholder.",
+    "term": 9,
+    "series": 0,
+    "inSequence": false,
+    "link":"link goes here",
+    "column":3,
+    "credits":3,
+    "weeks":7
+  },
+  {
+    "id": 39,//
+    "name": "PHW290",
+    "title": "Introduction to Data Management and Programming in SAS for Public Health",
+    "term": 3,
     "series": 0,
     "inSequence": false,
     "link":"link goes here",
@@ -53,411 +483,99 @@ CourseListing([
     "weeks":15
   },
   {
-    "id": 4,//
-    "name": "PHW142",
-    "title": "Introduction to Probability and Statistics",
-    "type":"Core/Breadth",
+    "id": 40,//
+    "name": "Fall Full-Term Elective",
+    "title": "Click change to select elective course",
     "term": 0,
     "series": 0,
     "inSequence": false,
     "link":"link goes here",
-    "column":0,
-    "credits":4,
-    "weeks":15
+    "column":1,
+    "credits":0,
+    "weeks":0
   },
   {
-    "id": 5,//
-    "name": "PHW250",
-    "title": "Epidemiologic Methods",
-    "type":"Core/Breadth",
-    "term": 0,
+    "id": 41,//
+    "name": "Fall-1 Elective",
+    "title": "Click change to select elective course",
+    "term": 1,
     "series": 0,
     "inSequence": false,
     "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":15
+    "column":1,
+    "credits":0,
+    "weeks":0
   },
   {
-    "id": 6,//
-    "name": "PHW289",
-    "title": "Interdisciplinary Seminar",
-    "type":"Core/Breadth",
-    "term": 0,
+    "id": 42,//
+    "name": "Fall-2 Elective",
+    "title": "Click change to select elective course",
+    "term": 2,
     "series": 0,
     "inSequence": false,
     "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":7
-  },
-  // HEALTH POLICY AND MANAGEMENT COURSES
-  {
-    "id": 7,//
-    "name": "PHW227A",
-    "title": "Health Care Finance",
-    "type":"HealthPolicyManagement",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":15
+    "column":1,
+    "credits":0,
+    "weeks":0
   },
   {
-    "id": 8,//
-    "name": "PHW224",
-    "title": "Organizational Behavior and Management in Health Care",
-    "type":"HealthPolicyManagement",
-    "term": 0,
+    "id": 43,//
+    "name": "Spring Full-Term Elective",
+    "title": "Click change to select elective course",
+    "term": 3,
     "series": 0,
     "inSequence": false,
     "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":15
+    "column":1,
+    "credits":0,
+    "weeks":0
   },
   {
-    "id": 9,//
-    "name": "PHW220M",
-    "title": "Health Policy Methods",
-    "type":"HealthPolicyManagement",
-    "term": 0,
+    "id": 44,//
+    "name": "Spring-1 Elective",
+    "title": "Click change to select elective course",
+    "term": 4,
     "series": 0,
     "inSequence": false,
     "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":8
+    "column":1,
+    "credits":0,
+    "weeks":0
   },
   {
-    "id": 10,//
-    "name": "PHW226A",
-    "title": "Health Economics",
-    "type":"HealthPolicyManagement",
-    "term": 0,
+    "id": 45,//
+    "name": "Spring-2 Elective",
+    "title": "Click change to select elective course",
+    "term": 5,
     "series": 0,
     "inSequence": false,
     "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":15
+    "column":1,
+    "credits":0,
+    "weeks":0
   },
   {
-    "id": 11,//
-    "name": "PHW226C",
-    "title": "Economics of Population Health",
-    "type":"HealthPolicyManagement",
-    "term": 0,
+    "id": 46,//
+    "name": "Summer-1 Elective",
+    "title": "Click change to select elective course",
+    "term": 7,
     "series": 0,
     "inSequence": false,
     "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":15
+    "column":1,
+    "credits":0,
+    "weeks":0
   },
   {
-    "id": 12,//
-    "name": "PHW226F",
-    "title": "Economics of Population Health",
-    "type":"HealthPolicyManagement",
-    "term": 0,
+    "id": 47,//
+    "name": "Summer-2 Elective",
+    "title": "Click change to select elective course",
+    "term": 8,
     "series": 0,
     "inSequence": false,
     "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":15
-  },
-  {
-    "id": 13,//
-    "name": "PHW223",
-    "title": "Strategy in Health Care Organizations",
-    "type":"HealthPolicyManagement",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":7
-  },
-  // GLOBAL HEALTH COURSES
-  {
-    "id": 14,//
-    "name": "PHW209",
-    "title": "Comparative Health Systems",
-    "type":"GlobalHealth",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":8
-  },
-  {
-    "id": 15,//
-    "name": "PHW212",
-    "title": "Foundations of Global Health",
-    "type":"GlobalHealth",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":8
-  },
-  {
-    "id": 16,//
-    "name": "PHW213",
-    "title": "Global Health Ethics",
-    "type":"GlobalHealth",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":8
-  },
-  // EPIDEMIOLOGY AND BIOSTATISTICS COURSES
-  {
-    "id": 17,//
-    "name": "PHW236A",
-    "title": "Regulatory Science, Drug Development, and Public Health",
-    "type":"Epi/Bios",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":8
-  },
-  {
-    "id": 18,//
-    "name": "PHW241R",
-    "title": "Statistical Analysis of Categorical Data",
-    "type":"Epi/Bios",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":4,
-    "weeks":15
-  },
-  {
-    "id": 19,//
-    "name": "PHW250B",
-    "title": "Epidemiologic Methods II",
-    "type":"Epi/Bios",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":4,
-    "weeks":15
-  },
-  {
-    "id": 20,//
-    "name": "PHW251",
-    "title": "R for Public Health",
-    "type":"Epi/Bios",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":2,
-    "weeks":15
-  },
-  {
-    "id": 21,//
-    "name": "PHW252",
-    "title": "Epidemiological Analysis",
-    "type":"Epi/Bios",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":4,
-    "weeks":15
-  },
-  {
-    "id": 22,//
-    "name": "PHW252C",
-    "title": "Interventional Trial Design",
-    "type":"Epi/Bios",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":15
-  },
-  {
-    "id": 23,//
-    "name": "PHW253",
-    "title": "Outbreak Investigations",
-    "type":"Epi/Bios",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":7
-  },
-  {
-    "id": 24,//
-    "name": "PHW257",
-    "title": "Public Health Preparedness and Emergency Response",
-    "type":"Epi/Bios",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":7
-  },
-  {
-    "id": 25,//
-    "name": "PHW260",
-    "title": "Infectious Diseases",
-    "type":"Epi/Bios",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":7
-  },
-  {
-    "id": 26,//
-    "name": "PH290",
-    "title": "Introduction to Data Management and Programming in SAS for Public Health",
-    "type":"Epi/Bios",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":4,
-    "weeks":15
-  },
-  // SPATIAL DATA SCIENCE FOR PUBLIC HEALTH COURSES
-  {
-    "id": 27,//
-    "name": "PHW272A",
-    "title": "Intro to GIS for Public Health",
-    "type":"SpatialDS",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":8
-  },
-  {
-    "id": 28,//
-    "name": "PHW277",
-    "title": "GIS for Public Health Practice",
-    "type":"SpatialDS",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":8
-  },
-  {
-    "id": 29,//
-    "name": "PHW272C",
-    "title": "Applied Spatial Data Science for Public Health",
-    "type":"SpatialDS",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":8
-  },
-  // COMMUNITY HEALTH SCIENCES COURSES
-  {
-    "id": 30,//
-    "name": "PHW218",
-    "title": "Evaluation of Health and Social Programs",
-    "type":"CommunityHealth",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":7
-  },
-  {
-    "id": 31,//
-    "name": "PHW219",
-    "title": "Social and Behavioral Health Research: Introduction to Survey Methods",
-    "type":"CommunityHealth",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":7
-  },
-  {
-    "id": 32,//
-    "name": "PHW204",
-    "title": "Mass Communication in Public Health",
-    "type":"CommunityHealth",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":6
-  },
-  {
-    "id": 33,//
-    "name": "PHW206",
-    "title": "Maternal and Child Health Nutrition",
-    "type":"CommunityHealth",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":6
-  },
-  {
-    "id": 34,//
-    "name": "PHW205",
-    "title": "Program Planning",
-    "type":"CommunityHealth",
-    "term": 0,
-    "series": 0,
-    "inSequence": false,
-    "link":"link goes here",
-    "column":0,
-    "credits":3,
-    "weeks":7
-  },
+    "column":1,
+    "credits":0,
+    "weeks":0
+  }
 ])
