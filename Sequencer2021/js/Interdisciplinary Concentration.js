1efcb4b0741f9965b2cf7babdbeaec7429eef6ff
Richard Lin <rlin24@berkeley.edu>
2021-08-19 17:37:46 -0700

    added interdisciplinary classes

---
+++ Sequencer2021/js/Interdisciplinary Concentration.js
@@ -0,0 +1,59 @@
+// Interdisciplinary Concentration
+
+// Health Management Courses
+var PHW223 = {
+    name: "Strategic Management & the Health Sector",
+    term: "Summer 2",
+    units: 3,
+    color: "Red",
+    type: "HPM Concentration"
+  };
+
+  var PHW224 = {
+    name: "Organizational Behavior & Management in Healthcare",
+    term: "Fall 1",
+    units: 3,
+    color: "Red",
+    type: "HPM Concentration"
+  };
+
+  var PHW227A = {
+    name: "Healthcare Finance",
+    term: "Spring 2",
+    units: 3,
+    color: "Red",
+    type: "HPM Concentration"
+  };
+
+// Health Policy and Economics Courses
+var PHW220M = {
+    name: "Health Policy Methods",
+    term: "Fall 2",
+    units: 3,
+    color: "Red",
+    type: "HPM Concentration"
+  };
+
+var PHW226C = {
+    name: "Economics of Population Health",
+    term: "Spring 1",
+    units: 3,
+    color: "Red",
+    type: "HPM Concentration"
+};
+
+var PHW226A = {
+    name: "Health Economics",
+    term: "Fall 2",
+    units: 3,
+    color: "Red",
+    type: "HPM Concentration"
+};
+
+var PHW226F = {
+    name: "Cost-Effectiveness Analysis",
+    term: "Spring 1",
+    units: 1,
+    color: "Orange",
+    type: "Regulatory Science"
+};
